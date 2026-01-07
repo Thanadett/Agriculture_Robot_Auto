9d9640fcd63825f845db834066c10875bf1357e5
Thanadett <tanapremin@gmail.com>
2026-01-07 17:58:35 +0700

    Edits

--- ros2_ws/src/robot_bringup/launch/bringup.launch.py
+++ ros2_ws/src/robot_bringup/launch/bringup.launch.py
@@ -1,7 +1,4 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -11,21 +8,6 @@ def generate_launch_description():
 
     pkg_share = get_package_share_directory('robot_bringup')
     ekf_config = os.path.join(pkg_share, 'config', 'ekf.yaml')
-
-    use_teleop = LaunchConfiguration('use_teleop')
-    use_rviz = LaunchConfiguration('use_rviz')
-
-    declare_use_teleop = DeclareLaunchArgument(
-        'use_teleop',
-        default_value='true',
-        description='Enable keyboard teleoperation'
-    )
-
-    declare_use_rviz = DeclareLaunchArgument(
-        'use_rviz',
-        default_value='true',
-        description='Launch RViz'
-    )
 
     # ================= Robot Localization (EKF) =================
     ekf_node = Node(
@@ -48,30 +30,7 @@ def generate_launch_description():
         ]
     )
 
-    # ================= Teleop (optional) =================
-    teleop_node = Node(
-        package='teleop_twist_keyboard',
-        executable='teleop_twist_keyboard',
-        name='teleop_keyboard',
-        output='screen',
-        condition=IfCondition(use_teleop)
-    )
-
-    # ================= RViz (optional) =================
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        condition=IfCondition(use_rviz)
-    )
-
     return LaunchDescription([
-        declare_use_teleop,
-        declare_use_rviz,
-
         ekf_node,
         static_tf_imu,
-        teleop_node,
-        rviz_node
     ])
