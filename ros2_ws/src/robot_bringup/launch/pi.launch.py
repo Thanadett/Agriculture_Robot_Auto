3923c9478143988fbd30a37ea0e7b671887edd43
Thanadett <tanapremin@gmail.com>
2026-03-11 23:01:07 +0700

    launch

--- ros2_ws/src/robot_bringup/launch/pi.launch.py
+++ ros2_ws/src/robot_bringup/launch/pi.launch.py
@@ -3,23 +3,21 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    
-    # ── Camera 1: Pi Camera ──────────────────────────────────
+
     pi_camera_node = Node(
-        package='robot_bringup', 
-        executable='pi_camera_publisher', 
+        package='robot_bringup',
+        executable='pi_camera_publisher',
         parameters=[{
             'camera_id': '/dev/webcam_EYD_2k',
             'image_width': 320,
             'image_height': 240,
-            'fps': 20,
+            'fps': 12,
             'jpeg_quality': 60,
             'publish_raw': False,
             'use_x11_debug': False
         }]
     )
 
-    # ── Camera 2: Side Camera ────────────────────────────────
     side_camera_node = Node(
         package='robot_bringup',
         executable='cabbage_camera_publisher',
